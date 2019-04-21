a5032fa0dcd96c7c132dc908b10715ac17128ca4
Nuno Fidalgo <nunorfidalgo@gmail.com>
2019-04-21 16:55:14 +0100

    - Implementei a barreira com movimento esquerda/direita; - Terminar o cliente com a tecla ESQ; - Adicionei alguns "tijolos", mas não verifco se a bola bate neles;

--- cliente/cliente.cpp
+++ cliente/cliente.cpp
@@ -20,57 +20,110 @@ int _tmain(int argc, LPTSTR argv[])
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
 
+	int x, y;
+
 	_tprintf(TEXT("%s iniciou...\n"), SERVIDOR);
 	AcedeMemoriaPartilhadaJogo();
 
 	_tprintf(TEXT("%s teste de ção...\n"), SERVIDOR);
 
 	system("cls");
-
-	for (int x = 0; x < COLUNAS; x++) {
+	// Limites do jogo
+	for (x = 0; x < COLUNAS; x++) {
 		gotoxy(x, 0);
 		_tprintf(TEXT("-"));
 		gotoxy(x, LINHAS + 1);
 		_tprintf(TEXT("-"));
 	}
-	for (int y = 0; y <= LINHAS + 1; y++) {
+	for (y = 0; y <= LINHAS + 1; y++) {
 		gotoxy(0, y);
 		_tprintf(TEXT("|"));
 		gotoxy(COLUNAS, y);
 		_tprintf(TEXT("|"));
 	}
 
-	int x = COLUNAS / 2;
-	int y = LINHAS - 1;
+	// Tijolos
+	for (y = 4; y < 7; y++)
+		for (x = 6; x < COLUNAS - 6; x++) {
+			gotoxy(x, y);
+			_tprintf(TEXT("#"));
+		}
+	for (y = 8; y < 11; y++)
+		for (x = 6; x < COLUNAS - 6; x++) {
+			gotoxy(x, y);
+			_tprintf(TEXT("&"));
+		}
+
+	gotoxy(COLUNAS + 3, 0);
+	_tprintf(TEXT("ESC - sair"));
+
+	// posição inicial da bola
+	x = COLUNAS / 2, y = LINHAS;
+	int xd = 1, yd = 1;
 	gotoxy(x, y);
 	_tprintf(TEXT("*"));
 
-	int xd = 1 * 1;
-	int yd = 1 * 1;
+	// barreira do jogador, posição inicial da barreira
+	int xp = (COLUNAS / 2) - 4, yp = LINHAS, xpa;
+	gotoxy(xp, yp);
+	_tprintf(TEXT("____"));
+
+	// teclas
+	int key_input;
 
 	while (1) {
+		// apaga a posição anterior
 		gotoxy(x, y);
 		_tprintf(TEXT(" "));
-
-		x += xd;
-		y += yd;
-
-		if (x > COLUNAS - 2 || x < 2) {
-			xd *= 1 * -1;
+		x -= xd;
+		y -= yd;
+		if (x > COLUNAS - 2 || x < 2) { // limites direita e esquerda
+			xd *= -1;
 		}
-
-		if (y >= LINHAS || y < 2) {
-			yd *= 1 * -1;
+		if (y >= LINHAS || y < 2) { // limites inferior e superior
+			yd *= -1;
 		}
 		gotoxy(x, y);
 		_tprintf(TEXT("*"));
-		Sleep(20);
+
+		key_input = _gettch();
+		key_input = toupper(key_input);
+		_flushall();
+
+		switch (key_input) {
+		case 77: //direta
+			if (xp < COLUNAS - 4) {
+				xpa = xp;
+				xp++;
+				gotoxy(xpa, yp);
+				_tprintf(TEXT("    "));
+				gotoxy(xp, yp);
+				_tprintf(TEXT("____"));
+				gotoxy(COLUNAS + 3, 1);
+				_tprintf(TEXT("Tecla -> DIR: xp: %d, xpa: %d"), xp, xpa);
+			}
+			break;
+		case 75: // esquerda
+			if (xp > 1) {
+				xpa = xp;
+				xp--;
+				gotoxy(xpa, yp);
+				_tprintf(TEXT("    "));
+				gotoxy(xp, yp);
+				_tprintf(TEXT("____"));
+				gotoxy(COLUNAS + 3, 1);
+				_tprintf(TEXT("Tecla -> ESQ: xp: %d, xpa: %d"), xp, xpa);
+			}
+			break;
+		case 27: // ESC = sair
+			gotoxy(0, LINHAS + 2);
+			exit(1);
+		default:
+			break;
+		}
+		Sleep(50);
 	}
-	//gotoxy(COLUNAS + 1, LINHAS + 1);
 	gotoxy(0, LINHAS + 1);
-
-
 	//_gettchar();
-
 	return 0;
 }
